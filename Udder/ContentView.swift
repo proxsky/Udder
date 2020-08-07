b582af1cde89f080dc79b7e93e586eb969e6c171
Ricky <xproxskyx@gmail.com>
2020-08-07 16:50:47 -0400

    ???

---
+++ Udder/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  Udder
+//
+//  Created by R H on 8/7/20.
+//  Copyright © 2020 R H. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Udder")
+            Text("Car")
+            Text("GOGOGO")
+            sdfksdfosdfk oiksdf
+            
+            sdokfdspofksdopfk
+        }
+        
+        
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
